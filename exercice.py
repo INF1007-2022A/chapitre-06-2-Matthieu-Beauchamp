012c2fcdf4634661f4d644ef8a5966bca47785b8
Matthieu Beauchamp-Boulay <Matthieu_927@hotmail.ca>
2022-10-19 10:40:24 -0400

    Finished exercices

--- exercice.py
+++ exercice.py
@@ -5,27 +5,45 @@ from matplotlib.colors import cnames
 
 
 def list_to_dict(some_list: list) -> dict:
-    # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
+    # TODO: Transformer la liste en dictionnaire, 
+    # les éléments de la liste deviennent les clés et 
+    # leur index deviennent les valeurs
 
-    return {}
+    return {item: index for index, item in enumerate(some_list)}
 
 
 def color_name_to_hex(colors: list) -> list:
-    # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
+    # TODO: Trouver la valeur hex de chaque couleur 
+    # dans la liste et créer une liste de tuples où 
+    # le premier élément est le nom de la couleur et 
+    # le deuxième est la valeur hex
 
-    return []
+    # Wow that is useless!
+    return [(c, cnames[c])for c in colors]
+
 
 
 def create_list() -> list:
-    # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
+    # TODO: Créer une liste des 10 000 premiers entiers positif, 
+    # sauf pour les entiers de 15 à 350
 
-    return []
+    return [i for i in range(10001) if not (15 <= i <= 350) ]
 
 
 def compute_mse(model_dict: dict) -> dict:
-    # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
+    # TODO: Calculer l'erreur quadratique moyen pour chaque 
+    # modèle. Retourner un dictionnaire contenant les MSE.
 
-    return {}
+    # Whatever that is...
+
+    for model_key, model_value in model_dict.items():
+        err = 0
+        for elem in model_value:
+            err += (elem[1] - elem[0])**2
+
+        model_dict[model_key] = err / len(model_value)
+
+    return model_dict
 
 
 def main() -> None:
